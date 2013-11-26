296ee42733c937be83f2d58df8fce7e36c2f5b87
Michał Pawelski <kontakt@proapi.eu>
2013-11-26 21:10:59 +0100

    initial project commit after rails new

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+GithubDemo::Application.config.secret_key_base = '79fd0ec4537728d391a6fb9cd36cc8425b96c74f88eee8d76ac8529deaee2c0bf6d8b9d18b2713aaa81b36023e1a14337da63d5b14c1cd0748ed0890fec8236e'
